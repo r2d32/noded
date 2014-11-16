1af9279ff532aa995c3cdf14aa1bc7d5477a738c
r3d32 <cagudo@lion.lmu.edu>
2014-11-16 15:33:32 -0800

    new codeship build

--- start.js
+++ start.js
@@ -1,4 +1,8 @@
-var app = require("./app");
 var port = process.env.PORT || 5000;
+
+var http = require('http'),
+    tasks = require('./data'),
+    db = require('./db'),
+    app = require('./app')(tasks, db);
 
 app.listen(port)
